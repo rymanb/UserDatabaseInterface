1475ea25b9cba5931bc347e6683af7ece545ce67
Rymanb <ryman.b@digipen.edu>
2024-10-31 15:04:54 -0700

    added email to user data

--- UserDbInterface/UserMetadata.cs
+++ UserDbInterface/UserMetadata.cs
@@ -12,11 +12,13 @@ public class UserMetadata
     // Note that "id" must be lower case for the Cosmos APIs to work
     // and for consistency, all keys are lower case
     public string id { get { return GenerateId(); } }
+    
+    public string email { get; set; } = string.Empty;
 
     public string userid { get; set; } = string.Empty;
 
     public override string ToString()
     {
-        return $"id: {id}, userid: {userid}";
+        return $"id: {id}, userid: {userid}, email: {email}";
     }
 }
