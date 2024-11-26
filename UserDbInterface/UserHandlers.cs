f807e3b1dfd811f71a8a1568c0b56525d852feec
Rymanb <ryman.b@digipen.edu>
2024-11-26 12:15:32 -0800

    removed /

--- UserDbInterface/UserHandlers.cs
+++ UserDbInterface/UserHandlers.cs
@@ -144,15 +144,16 @@ public class Handlers
 
                 string secretValue = secret.Value;
 
-                // // get header
-                // string header = context.Request.Headers["x-auth-key"];
+                // get header
+                string header = context.Request.Headers["x-auth-key"];
 
-                // if (header != secretValue)
-                // {
-                //     throw new UserErrorException("Invalid x-auth-key");
-                // }
+                if (header != secretValue)
+                {
+                    log.LogUserError("Invalid x-auth-key - expected: " + secretValue + " received: " + header);
+                    throw new UserErrorException("Invalid x-auth-key");
+                }
 
-                /
+                
 
                 // Get the user id from the query string
                 string userId = GetParameterFromList("userid", context.Request, log);
